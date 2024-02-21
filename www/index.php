64dc8b2c79eaeac0f47b32dd8fe799d9d97c33c9
Harduin_Hero <harduinero@gmail.com>
2024-02-21 23:03:55 +0100

    structure html et style basic ok pour page de listing principale

---
+++ www/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SNDT</title>
+
+    <link rel="stylesheet" href="static/css/clear_theme.css">
+    <link rel="stylesheet" href="static/css/basic.css">
+
+</head>
+<body>
+    
+<header>
+    <h1>Simple Network Documentation Tool</h1>
+</header>
+
+<section class=center_container>
+    <table>
+            <tr>
+                <th colspan=3>&nbsp;</th>
+            </tr>
+            <tr>
+                <td>
+                    <label for="search_filter">Search</label>
+                </td>
+                <td colspan=2>
+                    <input type="text" id="search_filter">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Element type
+                </td>
+                <td colspan=2>
+                    <select>
+                        <option value="rack">Rack</option>
+                        <option value="device">Device</option>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <th>Technical name</th>
+                <th>Common name</th>
+                <th>Description</th>
+            </tr>
+            <tbody class="alt-row">
+                <tr>
+                    <td>?????</td>
+                    <td>?????</td>
+                    <td>?????</td>    
+                </tr>
+            </tbody>
+    </table>
+</section>
+
+</body>
+</html>
